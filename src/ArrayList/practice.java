f3aa19d8699599d08b8ba823eba2a2ca850f740c
raj <rajpat808@gmail.com>
2025-11-05 08:06:24 -0800

    make change in the coode

--- src/ArrayList/practice.java
+++ src/ArrayList/practice.java
@@ -6,12 +6,6 @@ public class practice {
          int b = 20;
 
          System.out.println(a + b);
-<<<<<<< HEAD
-         int d=40;
-         System.out.println(d);
-=======
-         int c=30;
-         System.out.println(c);
->>>>>>> master
+
     }
 }
