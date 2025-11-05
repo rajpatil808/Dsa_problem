20837d637bae2141cccc36d02f5e6a2df8562caf
raj <rajpat808@gmail.com>
2025-11-05 10:17:29 -0800

    ArrayList<ArrayList

--- src/ArrayList/practice.java
+++ src/ArrayList/practice.java
@@ -8,26 +8,47 @@ public class practice {
 
       Scanner in = new Scanner(System.in);
 
-      int n = in.nextInt();
 
-      ArrayList<Integer> arr = new ArrayList<>();
 
-      for(int i = 0; i < n; i++) {
-          int x = in.nextInt();
-          arr.add(x);
-      }
+     ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
 
-      for(int i = 0; i < n; i++) {
-          System.out.print(arr.get(i)+" ");
-      }
+     ArrayList<Integer> a = new ArrayList<>();
+     a.add(   1   );
 
+     ArrayList<Integer> b = new ArrayList<>();
+     b.add(  1  );
+     b.add(  1  );
+     b.add(  1  );
+
+     ArrayList<Integer> c = new ArrayList<>();
+     c.add( 1 );
+     c.add( 2 );
+     c.add( 2 );
+     c.add( 1 );
+
+     ArrayList<Integer> d = new ArrayList<>();
+     d.add(1);
+     d.add(3);
+     d.add(4);
+     d.add(3);
+     d.add(1);
+
+     arr.add(a);
+     arr.add(b);
+     arr.add(c);
+     arr.add(d);
+
+     for(int i = 0; i < arr.size(); i++) {
+         for(int j = 0; j < arr.get(i).size(); j++) {
+             System.out.print(arr.get(i).get(j)+" ");
+         }
          System.out.println();
+     }
 
-      arr.remove((arr.size() - 2));
 
-      for(int i = 0; i < n; i++) {
-          System.out.print(arr.get(i)+" ");
-      }
+
+
+
 
     }
 }
